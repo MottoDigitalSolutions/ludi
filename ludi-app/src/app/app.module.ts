91cc75ce4cc71a50bbd64605d8831675b2f2396e
cristians sanchez <cristiansleonardo@gmail.com>
2018-05-02 15:12:21 -0300

    test for carousel bootstrap

--- ludi-app/src/app/app.module.ts
+++ ludi-app/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { navComponent } from './layout/navbar.component';
 import { footerComponent } from './layout/footer.component';
+import { NgbdCarouselBasic } from './bootstrapComp/carousel-config';
 
 @NgModule({
   declarations: [
     AppComponent,
     navComponent,
-    footerComponent
+    footerComponent,
+    NgbdCarouselBasic
   ],
   imports: [
+    NgbModule.forRoot(),
+    HttpClientModule,
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
