178a99f69499601ba41cfe1fc1fdc96037fcba66
carlosblanco <carlosblancosierra@gmail.com>
2018-05-05 16:34:36 -0500

    added home section five to home.html and register in app module

--- ludi-app/src/app/app.module.ts
+++ ludi-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { DragScrollModule } from 'ngx-drag-scroll';
 import { SlickModule } from 'ngx-slick';
@@ -15,6 +15,7 @@ import { carouselHomeComponent } from './bootstrapComp/carousel-home.component';
 import { sectionTwoComponent } from './home/section-two.component';
 import { sectionThreeComponent } from './home/section-three.component';
 import { sectionFourComponent } from './home/section-four.component';
+import { sectionFiveComponent } from './home/section-five.component';
 import { PromocionesComponent } from './promociones/promociones.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -29,6 +30,7 @@ import { HomeComponent } from './home/home.component';
     sectionTwoComponent,
     sectionThreeComponent,
     sectionFourComponent,
+    sectionFiveComponent,
     PromocionesComponent,
     HomeComponent,
   ],
