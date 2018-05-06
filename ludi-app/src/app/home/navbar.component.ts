042076add1e4221fdd6eaa2711f4f4be5b4e57ca
cristians sanchez <cristiansleonardo@gmail.com>
2018-05-06 12:34:34 -0300

    integring RouterLink in the navbar

--- ludi-app/src/app/home/navbar.component.ts
+++ ludi-app/src/app/home/navbar.component.ts
@@ -7,29 +7,22 @@ import { Component } from '@angular/core';
 })
 
 export class navComponent {
-  correo  = 'informes@ludi.com.mx';
-  numero = '55 5545 7272';
-  
+    correo  = 'informes@ludi.com.mx';
+    numero = '55 5545 7272';
 
-  navInfo:any = [
-    {active: true, navElement: 'INICIO'},
-    {active: true, navElement:'PROMOCIONES'},
-    {active: false, navElement:'CHECK UPS'},
-    {active: true, navElement:'EMPRESAS'},
-    {active: true, navElement:'NOSOTROS'},
-    {active: true, navElement:'BLOG'},
-    {active: true, navElement:'CONTACTO'}
-  ];
-
-  navInfoDrow:any = [
-    {dropdownElement:'PREVENCION'},
-    {dropdownElement:'¿QUÉ ES UN CHECK UPS'},
-    {dropdownElement:'CHECK UP EMPRESARIAL'},
-    {dropdownElement:'CHECK UP INTEGRAL'},
-    {dropdownElement:'CHECK UP PLUS'},
-    {dropdownElement:'CHECK UP GENETEST'},
-    {dropdownElement:'CHECK UP ESPECIALIZADO'}
-
-  ];
+    inicio =  'INICIO';
+    prom = 'PROMOCIONES';
+    check = 'CHECK UPS';
+    empre = 'EMPRESAS';
+    nos = 'NOSOTROS';
+    blog = 'BLOG';
+    contact = 'CONTACTO';
+    preven = 'PREVENCION';
+    que = '¿QUÉ ES UN CHECK UPS';
+    empresarial = 'CHECK UP EMPRESARIAL';
+    integral = 'CHECK UP INTEGRAL';
+    plus = 'CHECK UP PLUS';
+    gene = 'CHECK UP GENETEST';
+    espe = 'CHECK UP ESPECIALIZADO';
 
 }
